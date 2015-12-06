7e4e1c4d1f5f04cbb45ff879dbf7eb5286c9cac9
Zordius Chen <zordius@yahoo-inc.com>
2015-12-06 21:10:21 +0800

    add eslint

--- build.js
+++ build.js
@@ -1,9 +1,11 @@
-var fs = require('fs');
-var handlebars = require('handlebars');
-
+// configs
 var out_dir = 'generated/';
 var in_dir = 'templates/';
 var partial_dir = 'partials/';
+
+// required libs
+var fs = require('fs');
+var handlebars = require('handlebars');
 
 var partials = fs.readdirSync(partial_dir).reduce(function (O, fn) {
     O[fn] = fs.readFileSync(partial_dir + fn, 'utf8');
