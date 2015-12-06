23902ba7690615a2d5732ee2b38a432d101fabc5
Zordius Chen <zordius@yahoo-inc.com>
2015-12-06 21:03:33 +0800

    build.js reads all partials

--- build.js
+++ build.js
@@ -1,1 +1,12 @@
-console.log('hello!');
+var fs = require('fs');
+var handlebars = require('handlebars');
+
+var out_dir = 'generated/';
+var in_dir = 'templates/';
+var partial_dir = 'partials/';
+
+var partials = fs.readdirSync(partial_dir).reduce(function (O, fn) {
+    O[fn] = fs.readFileSync(partial_dir + fn, 'utf8');
+    return O;
+}, {});
+
