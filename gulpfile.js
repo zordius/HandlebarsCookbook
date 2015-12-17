d422ed1f6014b64e01396fc6398b338aac303811
Zordius Chen <zordius@yahoo-inc.com>
2015-12-17 15:14:31 +0800

    move style into less file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
 var del = require('del');
 var eslint = require('gulp-eslint');
+var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
 var lint_files = ['*.js'];
+var less_files = ['*.less'];
 
 gulp.task('watch', ['build'], function () {
     gulp.watch(lint_files, ['lint', 'build']);
     gulp.watch(['partials/*', 'bookdata/*'], ['build']);
+});
+
+gulp.task('watchless', ['less'], function () {
+    gulp.watch(less_files, ['less']);
 });
 
 gulp.task('lint', function () {
@@ -15,11 +21,18 @@ gulp.task('lint', function () {
     .pipe(eslint.format());
 });
 
-gulp.task('clean', function () {
-    return del('generated/*');
+gulp.task('less', function () {
+    return gulp.src(less_files)
+    .pipe(less())
+    .pipe(gulp.dest('generated'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('build', ['clean'], function () {
+gulp.task('clean', function () {
+    return del('generated/*.html');
+});
+
+gulp.task('build', ['clean', 'less'], function () {
     delete require.cache[require.resolve('./build')];
     require('./build')();
     browserSync.reload();
@@ -34,4 +47,4 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('default', ['watch', 'browser-sync']);
+gulp.task('default', ['watch', 'watchless', 'browser-sync']);
